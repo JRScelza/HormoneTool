0e12a6d3e421b49d2a0672da4bd9823036cee90c
Jerome Scelza <jrscelza@Jeromes-MBP.fios-router.home>
2019-05-24 12:05:05 -0400

    buttons and graph fixed axis

--- src/dashboard_v2.py
+++ src/dashboard_v2.py
@@ -8,141 +8,401 @@ import numpy as np
 import pandas as pd
 
 
-
 # Create a DataFrame from the .csv file:
 df = pd.read_csv("../cycle_viz/mock_hormone_data.csv")
 ######-------------------------------------------########
-######-------------------------------------------########
+
 
 external_stylesheets =['https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-#
+
 app.css.append_css({'external_url': 'https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css'})  # noqa: E501
-#
-#
-app.layout = html.Div([
+
+
+                        
+
+app.layout = html.Div(
+                    style={'backgroundColor':'#F9F0DF',
+                           'margin':0
+
+                           },
+                    
+                    children = [
         
-    html.Button('Short', 
-                id='btn-1',
-                className = 'one columns',
-                n_clicks_timestamp= 0 ,
-                style = {
-                        'backgroundColor' :  '#4CAF50'
-                        }
-                ),
+        
+        
+                    html.Img(src=app.get_asset_url('logo.png'),style = {'width': 150}) ,
+                    
+                    html.A("About", target="_blank", href="https://www.oova.life", style={"color": "black", "text-decoration": "none", 'padding-left': 20, 'fontSize': 20}),
                 
-     html.Button('Long', 
-                 id='btn-2',
-                 className = 'one columns',
-                 n_clicks_timestamp= 0 ,
-                 style = {
-                        'backgroundColor' :  '#4CAF50'
-                        }
-                 ),
+                    
+                    html.P('''
+                           Let us help you understand
+                           ''',
+                           style = {
+                                 'fontSize' : 30,
+                                 'color': 'ff9900',
+                                 'font-family':'verdana',
+                                 'textAlign': "center",
+                                 'width': '100%',
+                                 'marginBottom': 30
+#                                 'padding-right': '30%',
+#                                 'padding-left': '30%'
+                                 
+                                     }
+                           ),
+                    
+#                    html.P('''
+#                           way that I can think
+#                           ''',
+#                           style = {
+#                                 'fontSize' : 20,
+#                                 'color': 'ff9900',
+#                                 'font-family':'verdana',
+#                                 'textAlign': "center",
+#                                 'width': '100%',
+#                                 'margin': 0
+##                                 'padding-right': '30%',
+##                                 'padding-left': '30%'
+#                                 
+#                                     }
+#                           ),
+#                    
+#                    html.P('''
+#                           to get this done right now
+#                           ''',
+#                           style = {
+#                                 'fontSize' : 20,
+#                                 'color': 'ff9900',
+#                                 'font-family':'verdana',
+#                                 'textAlign': "center",
+#                                 'width': '100%',
+#                                 'margin-bottom': 40
+##                                 'padding-right': '30%',
+##                                 'padding-left': '30%'
+#                                 
+#                                     }
+#                           ),
+
+                    html.Div(
+                            className = "row",
+                            children = [
+                                    
+                                    html.Div(
+                                            className = "three columns",
+                                            style = {
+                                                    'marginLeft' : 0,
+                                                    'marginRight' : 0,
+                                                    'borderTop':'3px solid #d6d6d6',
+                                                    'borderBottom':'3px solid #d6d6d6'
+
+
+                                                    },
+                                            children = [
+                                                    
+                                                    html.H2("Cycle Type",
+                                                                 style = {
+                                                                         'fontSize' : 20,
+                                                                         'color': 'ff9900',
+                                                                         'font-family':'verdana',
+                                                                         'textAlign': "left",
+                                                                         'margin': 10,
+                                                                         'padding-left': "20%"
+
+                                                                         }),
     
-    html.Button('PCOS', 
-                 id='btn-3',
-                 className = 'one columns',
-                 n_clicks_timestamp= 0,
-                 style = {
-                        'backgroundColor' :  '#4CAF50'
-                        }
-                 ),
+                                               
+                                                    html.Button('Short', 
+                                                        id='btn-1',
+                                                        className = 'one columns',
+                                                        n_clicks_timestamp= 0 ,
+                                                        ),
+       
+                                                   html.Button('Long', 
+                                                                 id='btn-2',
+                                                                 className = 'one columns',
+                                                                 n_clicks_timestamp= 0
+                                                                 ),
+                                                    
+                                                   html.Button('PCOS', 
+                                                                 id='btn-3',
+                                                                 className = 'one columns',
+                                                                 n_clicks_timestamp= 0
+                                                                 ),
+
+                                                               
+                                                   html.H2("Egg Release",
+                                                     style = {
+                                                             'fontSize' : 20,
+                                                             'color': 'ff9900',
+                                                             'font-family':'verdana',
+                                                             'textAlign': "left",
+                                                             'margin': 10,
+                                                             'padding-left': "20%"
+                                                            
+                                                             }),
     
-    html.Div([dcc.Graph(id='lh-graph')],
-              className = 'nine columns')
-    ]
-)
+                                               
+                                                    html.Button('Yes', 
+                                                        id='btn-4',
+                                                        className = 'one columns',
+                                                        n_clicks_timestamp= 0 ,
+                                                        ),
+       
+                                                   html.Button('No', 
+                                                                 id='btn-5',
+                                                                 className = 'one columns',
+                                                                 n_clicks_timestamp= 0
+                                                                 )
+                                                   
+
+                                                   ]),
+                                    html.Div(
+                                            className = "nine columns",
+                                            children = [
+                                                    dcc.Graph(id='lh-graph')
+                                                    ]
+                                            )
+                                        ]
+                                )
+                            ]
+                        )
+
 
 @app.callback([dash.dependencies.Output('lh-graph', 'figure'),
                dash.dependencies.Output('btn-1', 'style'),
                dash.dependencies.Output('btn-2', 'style'),
-               dash.dependencies.Output('btn-3', 'style')],
+               dash.dependencies.Output('btn-3', 'style'),
+               dash.dependencies.Output('btn-4', 'style'),
+               dash.dependencies.Output('btn-5', 'style')],
               [dash.dependencies.Input('btn-1', 'n_clicks_timestamp'),
                dash.dependencies.Input('btn-2', 'n_clicks_timestamp'),
-               dash.dependencies.Input('btn-3', 'n_clicks_timestamp')])
+               dash.dependencies.Input('btn-3', 'n_clicks_timestamp'),
+               dash.dependencies.Input('btn-4', 'n_clicks_timestamp'),
+               dash.dependencies.Input('btn-5', 'n_clicks_timestamp')])
 
-def display(btn1, btn2, btn3):
+def display(btn1, btn2, btn3, btn4, btn5):
     
     if int(btn1) > int(btn2) and int(btn1) > int(btn3):
         data = df['lh_short'].values
         
         style1 = {
-            'backgroundColor': "#C13605"
-            }
+            'backgroundColor': "#A52D04",
+            'color': '#FEF9DD',
+            'width':'90%',
+            'margin': '5%'            }
         
         style2 = {
-            'backgroundColor': "#FEF9DD"
-            }
+            'backgroundColor': "#FEF9DD",
+            'color': '#A52D04',
+            'width':'90%',
+            'margin': '5%'            }
                 
         style3 = {
-            'backgroundColor': "#FEF9DD"
-            }
+            'backgroundColor': "#FEF9DD",
+            'color': '#A52D04',
+            'width':'90%',
+            'margin': '5%'            }
         
         
     elif int(btn2) > int(btn1) and int(btn2) > int(btn3):
         data = df['lh_long'].values
         
         style1 = {
-            'backgroundColor': "#FEF9DD"
-            }
+            'backgroundColor': "#FEF9DD",
+            'color': '#A52D04',
+            'width':'90%',
+            'margin': '5%'            }
         
         style2 = {
-            'backgroundColor': "#C13605"
-            }
+            'backgroundColor': "#A52D04",
+            'color': '#FEF9DD',
+            'width':'90%',
+            'margin': '5%'            }
                 
         style3 = {
-            'backgroundColor': "#FEF9DD"
-            }
+            'backgroundColor': "#FEF9DD",
+            'color': '#A52D04',
+            'width':'90%',
+            'margin': '5%'            }
         
     elif int(btn3) > int(btn1) and int(btn3) > int(btn2):
         data = df['lh_pcos'].values
         
         style1 = {
-            'backgroundColor': "#FEF9DD"
-            }
+            'backgroundColor': "#FEF9DD",
+            'color': '#A52D04',
+            'width':'90%',
+            'margin': '5%'            }
         
         style2 = {
-            'backgroundColor': "#FEF9DD"
-            }
+            'backgroundColor': "#FEF9DD",
+            'color': '#A52D04',
+            'width':'90%',
+            'margin': '5%'            }
                 
         style3 = {
-            'backgroundColor': "#C13605"
-            }
+            'backgroundColor': "#A52D04",
+            'color': '#FEF9DD',
+            'width':'90%',
+            'margin': '5%'            }
         
     else:
         data = df['lh_normal'].values
         
         style1 = {
-            'backgroundColor': "#FEF9DD"
+            'backgroundColor': "#FEF9DD",
+            'color': '#A52D04',
+             'width':'90%',
+             'margin': '5%'
             }
         
         style2 = {
-            'backgroundColor': "#FEF9DD"
+            'backgroundColor': "#FEF9DD",
+            'color': '#A52D04',
+             'width':'90%',
+             'margin': '5%'
+
             }
                 
         style3 = {
-            'backgroundColor': "#FEF9DD"
+            'backgroundColor': "#FEF9DD",
+            'color': '#A52D04',
+            'width':'90%',
+            'margin': '5%'
+
             }
         
+    if int(btn4) > int(btn5):
+        pg_data = df['pg_normal'].values
+
+        
+        style4 = {
+            'backgroundColor': "#4B0082",
+            'color': '#FEF9DD',
+            'width':'90%',
+            'margin': '5%'
+
+            }
+        
+        style5 = {
+            'backgroundColor': "#FEF9DD",
+            'color': '#4B0082',
+            'width':'90%',
+            'margin': '5%'
+
+            }
+        
+    elif int(btn5) > int(btn4):
+        pg_data = df['pg_not'].values
+
+        
+        style4 = {
+            'backgroundColor': "#FEF9DD",
+            'color': '#4B0082',
+            'width':'90%',
+            'margin': '5%'
+
+            }
+        
+        style5 = {
+            'backgroundColor': "#4B0082",
+            'color': '#FEF9DD',
+            'width':'90%',
+            'margin': '5%'
+
+            }
+
+    else:
+        pg_data = df['pg_normal'].values
+
+        style4 = {
+            'backgroundColor': "#FEF9DD",
+            'color': '#4B0082',
+            'width':'90%',
+            'margin': '5%'
+
+            }
+        
+        style5 = {
+            'backgroundColor': "#FEF9DD",
+            'color': '#4B0082',
+            'width':'90%',
+            'margin': '5%'
+
+            }
+
+
     x = np.array(range(0, len(data)))
     y = data
-        
+    
+    data2 = df['lh_normal'].values
+    x2 = np.array(range(0, len(data2)))
+    y2 = data2
+    
+    
+    pg_x = np.array(range(0, len(pg_data)))
+    pg_y = pg_data
+    
+    pg_x_fx = np.array(range(0, len(df['pg_normal'].values)))
+    pg_y_fx = df['pg_normal'].values
+    
+    
     figure = {
             'data': [{
                 'type': 'scatter',
                 'x' : x,
                 'y' : y,
-                'name' : "Normal LH",
+                'name' : "Luteinizing",
+                'opacity' : 1.0,
+                
+                'line' : {
+                        'width' : 4,
+                        'color' : "#A52D04"
+                        }
+            },
+            
+            {
+                'type': 'scatter',
+                'x' : x2,
+                'y' : y2,
+                'name' : "Base",
                 'opacity' : 0.2,
                 
                 'line' : {
                         'width' : 4,
                         'color' : "#A52D04"
                         }
-
-            }],
+            },
+            
+                
+            {
+                'type': 'scatter',
+                'x' : pg_x,
+                'y' : pg_y,
+                'name' : "Progesterone",
+                'opacity' : 1.0,
+                
+                'line' : {
+                        'width' : 4,
+                        'color' : "#4B0082"
+                        }
+            },
+                
+            {
+                'type': 'scatter',
+                'x' : pg_x_fx,
+                'y' : pg_y_fx,
+                'name' : "Base",
+                'opacity' : 0.2,
+                
+                'line' : {
+                        'width' : 4,
+                        'color' : "#4B0082"
+                        }
+            }
+        
+            ],
     
             'layout': {
                 'paper_bgcolor' : '#FCF1DA',
@@ -152,16 +412,24 @@ def display(btn1, btn2, btn3):
                         'showticklabels' : False
                         },
                 'yaxis' : {
-                'showticklabels' : False
-                        }
+                    'showticklabels' : False,
+                    'range' : [0,26],
+                    'autorange' :False
+                        },
+                'height': 400
+
+
             }
     }
     
-    return figure, style1, style2, style3
+    return figure, style1, style2, style3, style4, style5
+
+
+
 
 
 if __name__ == '__main__':
-    app.run_server( port = 8921, debug=True)
+    app.run_server( port = 8928, debug=True)
 
 
 
